a0c5d19937f5947a03036cb161de9c5a58d3d9d7
Genesis Lara <genesislara@pursuit.org>
2023-07-09 21:14:12 -0400

    lab with Aisha

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
 const BASE_URL = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=boolean";
+console.log(BASE_URL)
 
+const submit = document.querySelector('button[type="submit"]');
+const main = document.querySelector("main");
 
-const button = document.querySelector("button");
-//const results = document.querySelector("results");
-const card = document.querySelector(".card");
-
-
-button.addEventListener("click", (e) => {
-    e.preventDefault();
-    //const newQuestion = results;
-    fetch(`${BASE_URL}`)
+submit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("firing click");
+  fetch(BASE_URL)
     .then(data => data.json())
-    .then(json => {
-      showCard(json)
-    })
-    .catch(err => showError(err))
-});
+    .then(json => showCard(json))
+    .catch(err => alert("Something went wrong.", err))
+})
 
 const showCard = (json) => {
-  card.innerHTML = `
-  <article class="card">
-  <h2>${json.results.category}</h2>
-  <p>${json.results.question}</p>
-  <button>${json.results.correct_answer} ${json.results.incorrect_answers}</button>
-  <p class="hidden">${json.results.correct_answer}</p>
-</article>
-  `
+  let results = json.results;
+  let questions = "";
+
+  for (let i = 0; i < results.legnth; i++) {
+    questions +=
+      `<h2>${results[i].category}</h2>
+    <p>${results[i].question}</p>
+    <button>${results[i].correct_answer} ${results[i].incorrect_answers}</button>
+    <p class="hidden">${results[i].correct_answer}</p>`
+  }
+  main.innerHTML = questions;
 }
